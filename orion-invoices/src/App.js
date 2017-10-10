5f9715d5ba45616907e6b85fd8dcfc9fb6ad88d1
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2017-10-10 22:47:56 +1300

    Created code base and added basic mockup of login panel

---
+++ orion-invoices/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Style/App.css';
+
+import Login from './Components/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Login />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ orion-invoices/src/Components/Login.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import LoginForm from './Forms/LoginForm';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        <LoginForm />
+      </div>
+    );
+  }
+
+}
+
+export default Login;
