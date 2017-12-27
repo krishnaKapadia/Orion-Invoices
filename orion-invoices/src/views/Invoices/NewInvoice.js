b220bc55e635341dcfd13933d46211b11392658e
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2017-12-27 17:43:19 +1300

    changed link hover style on sidebar menu

--- orion-invoices/src/views/Invoices/NewInvoice.js
+++ orion-invoices/src/views/Invoices/NewInvoice.js
@@ -7,6 +7,24 @@ import { NavLink } from 'react-router-dom';
 
 class NewInvoice extends Component {
 
+  // Endpoint to send to API: clientName and items object containing objects for each item.
+  // eg:
+  /*
+    clientName: "Bill",
+    items [
+      { desc: item1, quantity: 10, cost: 4.50 },
+      { desc: item2, quantity: 5, cost: 8.50 }
+    ]
+
+    On Form Submit, this object will be sent to API for storage
+
+    Possible methods:
+      - Directly send to API and therefore when the user navigates to the order page, it will have to do a fetch request every time.
+      - Get redux to do it while simulaniously updating its local state therefore not needed to fetch on every visit to the page.
+
+    Currently will use first option, but will explore redux at a later date as it is faster as components will access local store and API calls will only be made when that store is changed
+  */
+
   constructor(props) {
     super(props);
 
@@ -14,6 +32,9 @@ class NewInvoice extends Component {
     this.removeRow = this.removeRow.bind(this);
 
     this.state = {
+      clientName: "",
+      address: "",
+      clientNumber: "",
       rows: ['']
     }
   }
@@ -59,10 +80,16 @@ class NewInvoice extends Component {
                   {/* Client Details */}
                   <Col md="4">
                     <h5 className="bold">To:</h5>
-                    <p>Krishna Kapadia<br />
-                      55 Kanpur Road<br />
-                      Broadmeadows<br />
-                      Wellington, 6035</p>
+                    <Input type="text" id="clientName" placeholder="Enter the clients name" />
+                    <Input type="text" id="clientAddress" placeholder="Enter the clients address" />
+                    <Input type="text" id="clientSuburb" placeholder="Enter the clients suburb" />
+                    <Input type="text" id="clientCountry" placeholder="Enter the clients Country" />
+                    <p>
+                      {/* Krishna Kapadia<br /> */}
+                      {/* 55 Kanpur Road<br /> */}
+                      {/* Broadmeadows<br /> */}
+                      {/* Wellington, 6035 */}
+                    </p>
                   </Col>
 
                   <Col md="4">
